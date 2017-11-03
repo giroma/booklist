eb844edb2277f06fa138b3d7a1d175160fcb8647
Argir Argirov <dj.giro@gmail.com>
2017-11-03 16:32:50 -0400

    respond_to html added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    respond_to do |format|
+      format.html
+      # format.json { render json: @resource }
+    end
   end
-
 end
