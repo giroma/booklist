0867eb4b8f8e97d8d40440ff8619e567f4c0543c
Argir Argirov <dj.giro@gmail.com>
2017-11-03 16:34:52 -0400

    text response added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     respond_to do |format|
       format.html
+      format.text
       # format.json { render json: @resource }
     end
   end
