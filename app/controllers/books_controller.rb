9e879f0f211e96ba8351661e5a7ced9e8fcc695f
Argir Argirov <dj.giro@gmail.com>
2017-11-05 14:57:13 -0500

    text csv format responses added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,21 @@ class BooksController < ApplicationController
     respond_to do |format|
       format.html
       format.text
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
       # format.json { render json: @resource }
     end
   end
+  def self.generate_csv(book_list)
+  header = ['id', 'title', 'author', 'already_read']
+
+  CSV.generate(headers: true) do |csv|
+    csv << header
+
+    book_list.each do |book|
+      csv << [book.id, book.title, book.author, book.already_read]
+    end
+  end
+end
 end
